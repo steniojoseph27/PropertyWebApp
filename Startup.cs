67cf3527d366b7f518dfc4e47a1aff97771f8cc7
Stenio Joseph <steniojoseph@yahoo.com>
2021-02-04 15:15:37 -0400

    -- Make Properties page be the load page

--- Startup.cs
+++ Startup.cs
@@ -61,7 +61,7 @@ namespace PropertyWebApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Properties}/{action=Index}/{id?}");
             });
         }
     }
